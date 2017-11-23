04b6a5bc8ed0f056580348ea088b2474c235b873
emsk <emsk1987@gmail.com>
2017-11-23 23:35:21 +0900

    Add specs for `help` command

--- spec/brew_outdated_formatter/cli_spec.rb
+++ spec/brew_outdated_formatter/cli_spec.rb
@@ -1,2 +1,109 @@
 RSpec.describe BrewOutdatedFormatter::CLI do
+  let(:help) do
+    <<-EOS
+Commands:
+  #{command} help [COMMAND]          # Describe available commands or one specific command
+  #{command} output                  # Format output of `brew outdated`
+  #{command} version, -v, --version  # Print the version
+
+    EOS
+  end
+
+  shared_examples_for 'a `help` command' do
+    before do
+      expect(File).to receive(:basename).with($PROGRAM_NAME).and_return(command).at_least(:once)
+    end
+
+    it { is_expected.to output(help).to_stdout }
+  end
+
+  describe '.start' do
+    let(:command) { 'brew_outdated_formatter' }
+
+    subject { -> { described_class.start(thor_args) } }
+
+    context 'given `help`' do
+      let(:thor_args) { %w[help] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `--help`' do
+      let(:thor_args) { %w[--help] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `-h`' do
+      let(:thor_args) { %w[-h] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `h`' do
+      let(:thor_args) { %w[h] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `he`' do
+      let(:thor_args) { %w[he] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `hel`' do
+      let(:thor_args) { %w[hel] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `help output`' do
+      let(:thor_args) { %w[help output] }
+      let(:help) do
+        <<-EOS
+Usage:
+  #{command} output
+
+Options:
+  -f, [--format=FORMAT]          # Format. (markdown, json, yaml, csv, tsv, xml, html)
+                                 # Default: markdown
+  -p, [--pretty], [--no-pretty]  # `true` if pretty output.
+
+Format output of `brew outdated`
+        EOS
+      end
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `help version`' do
+      let(:thor_args) { %w[help version] }
+      let(:help) do
+        <<-EOS
+Usage:
+  #{command} version, -v, --version
+
+Print the version
+        EOS
+      end
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `help help`' do
+      let(:thor_args) { %w[help help] }
+      let(:help) do
+        <<-EOS
+Usage:
+  #{command} help [COMMAND]
+
+Describe available commands or one specific command
+        EOS
+      end
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `abc`' do
+      let(:thor_args) { %w[abc] }
+      it { is_expected.to output(%(Could not find command "abc".\n)).to_stderr }
+    end
+
+    context 'given `helpp`' do
+      let(:thor_args) { %w[helpp] }
+      it { is_expected.to output(%(Could not find command "helpp".\n)).to_stderr }
+    end
+  end
 end
