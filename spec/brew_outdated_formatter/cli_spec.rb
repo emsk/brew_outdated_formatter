0eee297364ffabee44a6fec5be5e70f72a91bbf9
emsk <emsk1987@gmail.com>
2017-11-30 20:29:38 +0900

    Add specs for Markdown format

--- spec/brew_outdated_formatter/cli_spec.rb
+++ spec/brew_outdated_formatter/cli_spec.rb
@@ -1,4 +1,22 @@
 RSpec.describe BrewOutdatedFormatter::CLI do
+  let(:stdin) do
+    <<-EOS
+test1 (0.0.1) < 0.1
+test2 (0.0.2) < 0.1 [pinned at 0.0.2]
+test3 (10.10.1_1) < 11.0.1_1
+    EOS
+  end
+
+  let(:stdout_markdown) do
+    <<-EOS
+| formula | installed | current | pinned |
+| --- | --- | --- | --- |
+| test1 | 0.0.1 | 0.1 | |
+| test2 | 0.0.2 | 0.1 | 0.0.2 |
+| test3 | 10.10.1_1 | 11.0.1_1 | |
+    EOS
+  end
+
   let(:help) do
     <<-EOS
 Commands:
@@ -7,6 +25,14 @@ Commands:
   #{command} version, -v, --version  # Print the version
 
     EOS
+  end
+
+  shared_examples_for 'markdown format' do
+    before do
+      stub_const('STDIN', StringIO.new(stdin))
+    end
+
+    it { is_expected.to output(stdout_markdown).to_stdout }
   end
 
   shared_examples_for 'a `help` command' do
@@ -21,6 +47,40 @@ Commands:
     let(:command) { 'brof' }
 
     subject { -> { described_class.start(thor_args) } }
+
+    context 'given `output --format markdown`' do
+      let(:thor_args) { %w[output --format markdown] }
+      it_behaves_like 'markdown format'
+
+      context 'without STDIN' do
+        it { is_expected.not_to output.to_stdout }
+      end
+    end
+
+    context 'given `output -f markdown`' do
+      let(:thor_args) { %w[output -f markdown] }
+      it_behaves_like 'markdown format'
+    end
+
+    context 'given `output --format markdown --pretty`' do
+      let(:thor_args) { %w[output --format markdown --pretty] }
+      it_behaves_like 'markdown format'
+    end
+
+    context 'given `output -f markdown -p`' do
+      let(:thor_args) { %w[output -f markdown -p] }
+      it_behaves_like 'markdown format'
+    end
+
+    context 'given `output`' do
+      let(:thor_args) { %w[output] }
+      it_behaves_like 'markdown format'
+    end
+
+    context 'given ``' do
+      let(:thor_args) { %w[] }
+      it_behaves_like 'markdown format'
+    end
 
     context 'given `version`' do
       let(:command) { 'brew_outdated_formatter' }
